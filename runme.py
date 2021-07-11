19cc14a1b11885bfe346b53226217a09e1c964da
gms2009 <41393593+gms2009@users.noreply.github.com>
2021-07-12 03:45:34 +0800

    Add files via upload

---
+++ runme.py
@@ -0,0 +1,15 @@
+#runme.py
+import torch
+import minimal
+
+m1 = minimal.minimal()
+m1.print_num_instances()
+
+m2 = minimal.minimal()
+m1.print_num_instances()
+
+m3 = minimal.minimal()
+m1.print_num_instances()
+
+del m2
+m1.print_num_instances()
