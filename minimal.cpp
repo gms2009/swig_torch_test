dc351ac91e2939d8e841bc34579d0a508c46f7ed
gms2009 <41393593+gms2009@users.noreply.github.com>
2021-07-12 03:46:31 +0800

    Add files via upload

---
+++ minimal.cpp
@@ -0,0 +1,12 @@
+//minimal.cpp
+#include <iostream>
+#include "minimal.h"
+
+int minimal::num_instances = 0;
+
+void minimal::print_num_instances()
+{
+	using namespace std;
+        cout << "test tt" << endl;
+	cout << this-> num_instances << endl;
+}
